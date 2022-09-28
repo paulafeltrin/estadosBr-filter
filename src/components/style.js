74ec4c3a568ef1efc0cacd2efcf7921424495bf7
Paula Cassia Feltrin <feltrin.paulacassia@gmail.com>
2022-09-28 14:58:38 -0300

    adicionando cor

--- src/components/style.js
+++ src/components/style.js
@@ -10,12 +10,15 @@ export const Container = styled.section`
     display: flex;
     justify-content: center;
     padding: 8vh 0;
+    background-color: #009c3b;
+    color: white;
 `
 
 export const Div = styled.div`
     width: 40%;
 
     h1{
+        font-size: 1.5rem;
         text-align: center;
         padding: 2vh 1.3vw;
     }
@@ -28,6 +31,7 @@ export const Div = styled.div`
     }
 `
 export const Li = styled.li`
+    font-size: 1.2rem;
     list-style: none;
     padding: 1.2vh 3vw;
 `
